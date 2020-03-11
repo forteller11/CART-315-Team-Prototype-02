a1fd687ee942d507235209f59e39a0feba5b11db
forteller11 <charly.yan.miller@gmail.com>
2020-03-11 01:51:17 -0400

    Added food judger scripts

--- Assets/Scripts/Judger.cs
+++ Assets/Scripts/Judger.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Helpers;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class Judger : MonoBehaviour
 {
     //TODO for all food overlapping certain collider, judge it on function call
+    public Vector2 ColliderOffset;
+    public Vector2 ColliderSize;
+
+    [ContextMenu("Score")]
+    public void Score()
+    {
+        var foods = FindAllFoodsOnPlate();
+        
+        JudgeFoods(foods);
+        
+        
+    }
+    List<Food> FindAllFoodsOnPlate()
+    {
+        var hitFoods = new List<Food>();
+
+        var hits = Physics2D.OverlapBoxAll(transform.position.ToVector2XY()+ColliderOffset, ColliderSize, transform.rotation.eulerAngles.z);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            var hitFood = hits[i].GetComponent<Food>();
+            if (hitFood != null)
+            {
+                Debug.Log(hitFood.Type);
+                hitFoods.Add(hitFood);
+            }
+                
+        }
+
+        return hitFoods;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position.ToVector2XY() + ColliderOffset, ColliderSize);
+    }
 
     void JudgeFoods(List<Food> foods)
     {
@@ -19,23 +58,26 @@ public class Judger : MonoBehaviour
                 int flagFoodType01 = 1 << (int) foods[i].Type;
                 int flagFoodType02 = 1 << (int) foods[j].Type;
                 int combinedFoodTypeFlag = flagFoodType01 | flagFoodType02;
-                Judge(combinedFoodTypeFlag);
+                
+                switch (combinedFoodTypeFlag)
+                {
+                    case 1 << (int) Food.FoodType.Peach | 1 << (int) Food.FoodType.Bread:
+                        Debug.Log("fruit sandwich");
+                        break;
+                    default:
+                        Debug.Log("Food combo not registered");
+                        break;
+            
+                }
             }
         }
     }
 
-    void Judge(int foodComboFlag)
+   
+
+    void Update()
     {
-        switch (foodComboFlag)
-        {
-            case (int) Food.FoodType.Peach | (int) Food.FoodType.Bread:
-                Debug.Log("fruit sandwich");
-                break;
-            default:
-                Debug.Log("Food combo not registered");
-                break;
-            
-        }
+        Score();
     }
     
 }
