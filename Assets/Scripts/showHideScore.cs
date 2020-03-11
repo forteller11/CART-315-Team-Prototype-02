eb8e5cb725f61d0aedea48b4a2caac016dbf1e5c
mortebouse <31903560+mortebouse@users.noreply.github.com>
2020-03-11 01:27:09 -0400

    added very basic popup score that shows up on screen and updates when  bool "timeToShowScore" is true (still needs to be edited tho)

---
+++ Assets/Scripts/showHideScore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showHideScore : MonoBehaviour
+{
+    public bool timeToShowScore;
+
+    private GameObject score;
+    public int scoreCreativity;
+    public int scoreTaste;
+    private Text scoreLabelC;
+    private Text scoreLabelT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    score = GameObject.Find("Score UI");
+
+    scoreCreativity = 0;
+    scoreTaste = 0;
+    scoreLabelC = GameObject.Find("Score Number Creativity").GetComponent<Text>();
+    scoreLabelT = GameObject.Find("Score Number Taste").GetComponent<Text>();
+
+    timeToShowScore = false;
+}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(timeToShowScore == true)
+        {
+            scoreLabelC.text = scoreCreativity.ToString();
+            scoreLabelT.text = scoreTaste.ToString();
+            score.SetActive(true);
+        } else
+        {
+            score.SetActive(false);
+        }
+
+    }
+}
